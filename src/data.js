352952d8b42b59cf03c4184d3538b918edf3bf27
Anas Parwez <anasparvez237@gmail.com>
2022-01-25 12:11:44 +0530

    initial commit

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    name: "Aryaman Pandey",
+    age: 24,
+    image:
+      "https://media-exp1.licdn.com/dms/image/C4E03AQGi-9710O5fQw/profile-displayphoto-shrink_200_200/0/1632820424805?e=1648684800&v=beta&t=nj545VHwfipUiETJnPgPCwbsAhqFAHBERbCW4cGw_0I",
+  },
+  {
+    id: 2,
+    name: "Danish Ali",
+    age: 21,
+    image:
+      "https://media-exp1.licdn.com/dms/image/D5603AQEAh06q7lp-2g/profile-displayphoto-shrink_200_200/0/1631364225185?e=1648684800&v=beta&t=XA4tk2HRGzfisvziLy-ontMMwFHcg7WupR-nouRiro8",
+  },
+  {
+    id: 3,
+    name: "Larry Little",
+    age: 36,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+  },
+  {
+    id: 4,
+    name: "Sean Walsh",
+    age: 34,
+    image:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+  },
+  {
+    id: 5,
+    name: "Amaan Arshad",
+    age: 25,
+    image:
+      "https://media-exp1.licdn.com/dms/image/C5603AQHVuNBrbFkCpA/profile-displayphoto-shrink_200_200/0/1636735123604?e=1648684800&v=beta&t=HV3qsodplc2INLBHnm6qgFfXfBGy51rHgTcfPdb3sUA",
+  },
+];
